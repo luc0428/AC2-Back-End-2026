757c1f014853bf2958e14f66d852150a0951786c
lucas <lunemeth2004@gmail.com>
2026-04-30 21:36:26 -0300

    fazendo os models

--- ac2/src/main/java/facens/ac2/model/FuncionarioModel.java
+++ ac2/src/main/java/facens/ac2/model/FuncionarioModel.java
@@ -1,5 +1,26 @@
 package facens.ac2.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class FuncionarioModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
 }
