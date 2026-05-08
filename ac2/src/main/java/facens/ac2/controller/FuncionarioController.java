e0df34048bfdc4fddc6fba25967d719b6912c9ab
Gabriel <ganemeth2004@gmail.com>
2026-05-07 21:40:13 -0300

    terminando

--- ac2/src/main/java/facens/ac2/controller/FuncionarioController.java
+++ ac2/src/main/java/facens/ac2/controller/FuncionarioController.java
@@ -4,6 +4,7 @@ package facens.ac2.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import facens.ac2.service.FuncionarioService;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/Funcionario")
 public class FuncionarioController {
 
